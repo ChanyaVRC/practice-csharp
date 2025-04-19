c265c60b2a0d2263ded87e99c2180c576925c2fc
ChanyaKushima <chanya.vrchat@gmail.com>
2025-04-20 01:28:56 +0900

    Add UnderstandingRuntimeType

---
+++ implementation.test/UnderstandingRuntimeTypeTest.cs
@@ -0,0 +1,35 @@
+﻿namespace Implementation.Test;
+
+[Trait("TestOf", nameof(UnderstandingRuntimeType))]
+public class UnderstandingRuntimeTypeTest
+{
+    [Fact]
+    public void Test_GetCompileTimeType()
+    {
+        Assert.Equal(typeof(int), UnderstandingRuntimeType.GetCompileTimeType(1));
+        Assert.Equal(typeof(uint), UnderstandingRuntimeType.GetCompileTimeType(1u));
+        Assert.Equal(typeof(ValueType), UnderstandingRuntimeType.GetCompileTimeType<ValueType>(1u));
+        Assert.Equal(typeof(string), UnderstandingRuntimeType.GetCompileTimeType(""));
+        Assert.Equal(typeof(object), UnderstandingRuntimeType.GetCompileTimeType<object>(""));
+        Assert.Equal(typeof(object), UnderstandingRuntimeType.GetCompileTimeType(new object()));
+        Assert.Equal(typeof(Class), UnderstandingRuntimeType.GetCompileTimeType(new Class()));
+        Assert.Equal(typeof(object), UnderstandingRuntimeType.GetCompileTimeType<object>(new Class()));
+        Assert.Equal(typeof(Class), UnderstandingRuntimeType.GetRuntimeType<Class>(null));
+    }
+
+    [Fact]
+    public void Test_GetRuntimeType()
+    {
+        Assert.Equal(typeof(int), UnderstandingRuntimeType.GetRuntimeType(1));
+        Assert.Equal(typeof(uint), UnderstandingRuntimeType.GetRuntimeType(1u));
+        Assert.Equal(typeof(uint), UnderstandingRuntimeType.GetRuntimeType<ValueType>(1u));
+        Assert.Equal(typeof(string), UnderstandingRuntimeType.GetRuntimeType(""));
+        Assert.Equal(typeof(string), UnderstandingRuntimeType.GetRuntimeType<object>(""));
+        Assert.Equal(typeof(object), UnderstandingRuntimeType.GetRuntimeType(new object()));
+        Assert.Equal(typeof(Class), UnderstandingRuntimeType.GetRuntimeType(new Class()));
+        Assert.Equal(typeof(Class), UnderstandingRuntimeType.GetRuntimeType<object>(new Class()));
+        Assert.Null(UnderstandingRuntimeType.GetRuntimeType<Class?>(null));
+    }
+
+    class Class { }
+}
---
+++ implementation/UnderstandingRuntimeType.cs
@@ -0,0 +1,17 @@
+﻿namespace Implementation;
+
+public class UnderstandingRuntimeType
+{
+    public static Type GetCompileTimeType<T>(T value)
+    {
+        // TODO: value のコンパイル時の型を取得して返す
+        throw new NotImplementedException();
+    }
+
+    public static Type? GetRuntimeType<T>(T value)
+    {
+        // TODO: value の実行時の型を取得して返す
+        // REMARK: 実行時の方がない場合は null を返す
+        throw new NotImplementedException();
+    }
+}

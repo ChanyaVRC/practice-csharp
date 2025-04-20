e6df17c8464cbf679279380871f1f0b74a0494aa
ChanyaKushima <chanya.vrchat@gmail.com>
2025-04-21 05:39:41 +0900

    Add a benchmark template

--- benchmark/Program.cs
+++ benchmark/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿extern alias Expected;
+extern alias Actual;
+
+using BenchmarkDotNet.Running;
+
+namespace Benchmark;
+
+public class Benchmarks
+{
+
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<Benchmarks>();
+    }
+}

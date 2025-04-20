b8dfc9f0c894115c2a441706da2879a73b612c82
ChanyaKushima <chanya.vrchat@gmail.com>
2025-04-20 19:29:21 +0900

    Add MakeClassIntroduction

---
+++ implementation.test/MakeClassIntroductionTest.cs
@@ -0,0 +1,144 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using Xunit.Sdk;
+
+namespace Implementation.Test;
+
+[Trait("TestOf", nameof(MakeClassIntroduction))]
+public class MakeClassIntroductionTest 
+{
+    private static readonly Type s_parentType = typeof(MakeClassIntroduction);
+
+    private static Type? GetUserClass()
+        => s_parentType.GetNestedType("User", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+    [Fact]
+    public void Test_ExistsUserClass()
+    {
+        Type? userClass = GetUserClass();
+        Assert.NotNull(userClass);
+    }
+
+    [Fact]
+    public void Test_ApplyingAppropriateModifiers()
+    {
+        Type? userClass = GetUserClass();
+        Assert.NotNull(userClass);
+        Assert.True(userClass.IsNestedPublic, "User class should be public");
+        Assert.False(userClass.IsAbstract, "User class should not be abstract");
+        Assert.False(userClass.IsSealed, "User class should not be sealed");
+        Assert.False(userClass.IsValueType, "User class should not be a value type");
+    }
+
+    [Fact]
+    public void Test_DefaultConstructor()
+    {
+        Type? userClass = GetUserClass();
+        Assert.NotNull(userClass);
+        ConstructorInfo? constructor = userClass.GetConstructor(Type.EmptyTypes);
+        Assert.NotNull(constructor);
+
+        object? instance = constructor.Invoke(null);
+        Assert.NotNull(instance);
+        Assert.IsType(userClass, instance);
+
+        FieldInfo? fieldInfo = userClass.GetField("_name", BindingFlags.NonPublic | BindingFlags.Instance);
+        Assert.NotNull(fieldInfo);
+        string? fieldValue = fieldInfo.GetValue(instance) as string;
+        Assert.Equal("unknown", fieldValue);
+    }
+
+    [Fact]
+    public void Test_ConstructorWithParameters()
+    {
+        Type? userClass = GetUserClass();
+        Assert.NotNull(userClass);
+        ConstructorInfo? constructor = userClass.GetConstructor([typeof(string)]);
+        Assert.NotNull(constructor);
+
+        object? instance = constructor.Invoke(["John Doe"]);
+        Assert.NotNull(instance);
+        Assert.IsType(userClass, instance);
+
+        FieldInfo? fieldInfo = userClass.GetField("_name", BindingFlags.NonPublic | BindingFlags.Instance);
+        Assert.NotNull(fieldInfo);
+        string? fieldValue = fieldInfo.GetValue(instance) as string;
+        Assert.Equal("John Doe", fieldValue);
+    }
+
+    [Fact]
+    public void Test_NameField()
+    {
+        Type? userClass = GetUserClass();
+        Assert.NotNull(userClass);
+
+        FieldInfo? fieldInfo = userClass.GetField("_name", BindingFlags.NonPublic | BindingFlags.Instance);
+        Assert.NotNull(fieldInfo);
+        Assert.Equal(typeof(string), fieldInfo.FieldType);
+        Assert.True(fieldInfo.IsPrivate, "_name field should be private");
+    }
+
+    [Fact]
+    public void Test_SetNameMethod()
+    {
+        Type? userClass = GetUserClass();
+        Assert.NotNull(userClass);
+        MethodInfo? methodInfo = userClass.GetMethod("SetName");
+        Assert.NotNull(methodInfo);
+
+        object? instance = Activator.CreateInstance(userClass);
+        Assert.NotNull(instance);
+        methodInfo.Invoke(instance, ["Jane Doe"]);
+
+        FieldInfo? fieldInfo = userClass.GetField("_name", BindingFlags.NonPublic | BindingFlags.Instance);
+        Assert.NotNull(fieldInfo);
+        string? fieldValue = fieldInfo.GetValue(instance) as string;
+        Assert.Equal("Jane Doe", fieldValue);
+    }
+
+    [Fact]
+    public void Test_NameProperty()
+    {
+        Type? userClass = GetUserClass();
+        Assert.NotNull(userClass);
+        
+        PropertyInfo? propertyInfo = userClass.GetProperty("Name");
+        Assert.NotNull(propertyInfo);
+
+        object? instance = Activator.CreateInstance(userClass);
+        Assert.NotNull(instance);
+        
+        MethodInfo? getMethod = propertyInfo.GetMethod;
+        MethodInfo? setMethod = propertyInfo.SetMethod;
+        Assert.NotNull(getMethod);
+        Assert.NotNull(setMethod);
+        Assert.True(getMethod.IsPublic, "The getter should be public");
+        Assert.True(setMethod.IsPrivate, "The setter should be private");
+
+        string? nameValue = getMethod.Invoke(instance, null) as string;
+        Assert.Equal("unknown", nameValue);
+
+        setMethod.Invoke(instance, ["John Doe"]);
+
+        nameValue = propertyInfo.GetValue(instance) as string;
+        Assert.Equal("John Doe", nameValue);
+    }
+
+    [Fact]
+    public void Test_NamePropertyAccessorsAreNotAutoImplemented()
+    {
+        var userClass = GetUserClass();
+        Assert.NotNull(userClass);
+
+        var propertyInfo = userClass.GetProperty("Name");
+        Assert.NotNull(propertyInfo);
+
+        var getMethod = propertyInfo.GetMethod;
+        var setMethod = propertyInfo.SetMethod;
+        Assert.NotNull(getMethod);
+        Assert.NotNull(setMethod);
+
+        Assert.False(getMethod.IsDefined(typeof(CompilerGeneratedAttribute), false), "The getter should not be auto‑implemented");
+        Assert.False(setMethod.IsDefined(typeof(CompilerGeneratedAttribute), false), "The setter should not be auto‑implemented");
+    }
+}
---
+++ implementation/MakeClassIntroduction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation;
+
+public class MakeClassIntroduction
+{
+    // TODO: "User" という名前のクラスを作成します
+    //       1. "User" クラスは "MakeClass" の外からアクセスできるようにします
+
+    // TODO: "User" クラスの中に "_name" という string 型のフィールドを作成します
+    //       1. "_name" フィールドは "User" クラスのコンストラクタで初期化します
+    //       2. "_name" フィールドは "User" クラスの外からアクセスできないようにします
+
+    // TODO: "User" クラスの中に "Name" という string 型のプロパティを作成します
+    //       1. backing field は "_name" を使用します
+    //       2. "Name" プロパティは "User" クラスの外からアクセスできるようにします
+    //       3. "Name" プロパティは "User" クラスの外から変更できないようにします
+    //       4. "Name" プロパティは "User" クラスの外から読み取れるようにします
+    //       5. "Name" プロパティは "User" クラス内部では書き換え可能にします
+
+    // TODO: "User" クラスの中に "SetName" というメソッドを作成します
+    //       1. "SetName" メソッドは "User" クラスの外からアクセスできるようにします
+    //       2. "SetName" メソッドは "User" クラスの外から呼び出せるようにします
+    //       3. "SetName" メソッドは 1つの string 型の引数を受け取ります
+    //       4. "SetName" メソッドは "_name" フィールドを引数で受け取った値に変更します
+    //       5. "SetName" メソッドは 戻り値を持たないようにします
+
+    // TODO: "User" クラスの中に 2つのコンストラクタを作成します
+    //       1. 1つ目のコンストラクタは string 型の引数を受け取ります
+    //       2. 1つ目のコンストラクタは "_name" フィールドを引数で受け取った値に変更します
+    //       3. 2つ目のコンストラクタは引数を受け取らないようにします
+    //       4. 2つ目のコンストラクタは "_name" フィールドを "unknown" という値に変更します
+    //       5. 2つ目のコンストラクタは ブロック内に処理を記載しないようにします
+}
